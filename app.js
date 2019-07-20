896af663ce138e395c7d35e146f1946304b1c79e
cckn <cckn1985@gmail.com>
2019-07-21 01:49:13 +0900

    add routes

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ import globalRouter from "./routers/globalRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 
+import routes from "./routes";
+
 const app = express();
 
 app.use(cookieParser());
@@ -15,8 +17,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(morgan("dev"));
 
-app.use("/", globalRouter);
-app.use("/user", userRouter);
-app.use("/video", videoRouter);
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 
 export default app;
--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -1,6 +1,14 @@
 import express from "express";
+import routes from "../routes";
 
 const router = express.Router()
+
+router.get(routes.home, (req,res)=>res.send("home"))
+router.get(routes.join, (req,res)=>res.send("join"))
+router.get(routes.login, (req,res)=>res.send("login"))
+router.get(routes.logout, (req,res)=>res.send("logout"))
+router.get(routes.search, (req,res)=>res.send("search"))
+
 
 
 export default router
